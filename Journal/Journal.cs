c48388678edff326d340956180d4e9fd02442595
Damián Garro <dgarro@dc.uba.ar>
2023-05-23 20:52:35 -0300

    Initial commit

---
+++ Journal/Journal.cs
@@ -0,0 +1,28 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace Journal;
+
+public class Journal : ModBehaviour
+{
+    private void Awake()
+    {
+        // You won't be able to access OWML's mod helper in Awake.
+        // So you probably don't want to do anything here.
+        // Use Start() instead.
+    }
+
+    private void Start()
+    {
+        // Starting here, you'll have access to OWML's mod helper.
+        ModHelper.Console.WriteLine($"My mod {nameof(Journal)} is loaded!", MessageType.Success);
+
+
+        // Example of accessing game code.
+        LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+        {
+            if (loadScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        };
+    }
+}
